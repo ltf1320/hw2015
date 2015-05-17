22757556379521f19923ecf4a4f359d100fef0cd
ltf1320 <327105504@qq.com>
2015-05-17 23:24:09 +0800

    del

--- testClient.sh
+++
@@ -1,5 +0,0 @@
-cd works/target
-for i in 1 2 3 4 5 6 7 8
-do
-  nohup ./game 127.0.0.1 6000 127.0.0.$i 600$i $i$i$i$i &
-done
--- testServer.sh
+++
@@ -1,4 +0,0 @@
-cd server
-./gameserver -gip 127.0.0.1 -seq replay -d 1 -m 10000 -b 50 -t 2000 -h 500  & 
-
-
