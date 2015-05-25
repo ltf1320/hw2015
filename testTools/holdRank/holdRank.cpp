fad9ecdf0fa5209a496abe022a98e4b2dd1a72f1
ltf1320 <327105504@qq.com>
2015-05-25 12:35:56 +0800

    holdRank

---
+++ testTools/holdRank/holdRank.cpp
@@ -0,0 +1,71 @@
+#include "dila.hpp"
+
+struct Hand
+{
+	int id;
+	int cards[7];
+	void common(int* commonCards)
+	{
+		for(int i=0;i<5;i++)
+			cards[i]=commonCards[i];
+	}
+	void hold(int card1,int card2)
+	{
+		cards[5]=card1;
+		cards[6]=card2;
+	}
+	void getHold(Dila *dila)
+	{
+		dila->deliverHandCard(cards+5);
+	}
+	static bool cmp(Hand a,Hand b)
+	{
+		bool res=Dila::pk(a.cards,b.cards)>0;
+		return res;
+	}
+};
+
+int win[52][52];
+
+bool sim(int hold1,int hold2,int playerNum)
+{
+	Dila dila;
+	dila.claimCard(hold1,hold2);
+	int common[5];
+	dila.deliverCard(5,common);
+	Hand* players=new Hand[playerNum];
+	players[0].common(common);
+	players[0].hold(hold1,hold2);
+	players[0].id=0;
+	for(int i=1;i<playerNum;i++)
+	{
+		players[i].common(common);
+		players[i].getHold(&dila);
+		players[i].id=i;
+	}
+	sort(players,players+playerNum,Hand::cmp);
+	if(players[0].id==0)
+		return true;
+	return false;
+}
+
+
+void MentoCarlo(int playerNum,int num)
+{
+	win[0][1]=0;
+	for(int i=0;i<num;i++)
+	{
+		if(sim(0,1,playerNum))
+			win[0][1]++;
+	}
+	printf("%d\n",win[0][1]);
+	printf("%f\n",1.0*win[0][1]/num);
+}
+
+int main()
+{
+	srand(time(0));
+	printf("%d\n",RAND_MAX);
+	MentoCarlo(8,10000);
+
+}
